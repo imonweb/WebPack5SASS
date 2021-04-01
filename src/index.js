4393c1d1c3e15d729dbdbed30ca7e73109c20475
Imon <contact@imonweb.co.uk>
2021-04-01 11:59:28 +0100

    SASS installed

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+import './style.scss';
+import './woof.css';
+
 const headline = "WebPack SASS";
 document.querySelector("h1").innerText = headline;
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(sc|c)ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+        
+      }
+    ],
+    
+  },
+  devtool: false,
+  
+};
